b93f6e62905891f88c8bc9a826da7f872eaaae27
tinyms <i@tinyms.com>
2011-05-20 10:18:47 +0800

    Signed-off-by: tinyms <i@tinyms.com>

---
+++ src/JsCompressor.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : JsCompressor.cpp
+// Author      : tinyms
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
